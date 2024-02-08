be57d6c45a32b2839f745758e210447edd389388
Adrian Lovera <105324331+loveradrian28@users.noreply.github.com>
2024-02-08 16:03:37 +0000

    Starwars blog

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,16 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useContext } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import { ListCards } from "../component/cards";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="text-center mt-5">
+      {["planets", "people", "vehicles"].map((element) => (
+        <ListCards key={element} element={element} />
+      ))}
+    </div>
+  );
+};
